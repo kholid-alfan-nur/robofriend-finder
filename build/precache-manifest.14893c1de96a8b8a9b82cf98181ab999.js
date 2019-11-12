122e7f33d566e0c032aea93664e2ece271fa74d8
Kholid Alfan Nur <kholidnur85@gmail.com>
2019-11-12 14:37:22 -0500

    first commit

---
+++ build/precache-manifest.14893c1de96a8b8a9b82cf98181ab999.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "e66458714fe55a32ab6b9318f9fbff48",
+    "url": "/index.html"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/static/css/2.eb45ad44.chunk.css"
+  },
+  {
+    "revision": "aa3dce75481fd2c6b80b",
+    "url": "/static/css/main.0113fac7.chunk.css"
+  },
+  {
+    "revision": "eb2e83d34c498388c016",
+    "url": "/static/js/2.591f2f3b.chunk.js"
+  },
+  {
+    "revision": "aa3dce75481fd2c6b80b",
+    "url": "/static/js/main.818268b2.chunk.js"
+  },
+  {
+    "revision": "9bf820bd41e6d656f8db",
+    "url": "/static/js/runtime-main.f59df13c.js"
+  },
+  {
+    "revision": "12f9bb12fbe355e1acc2a0cab0ca2ec1",
+    "url": "/static/media/robofont.12f9bb12.woff"
+  }
+]);
